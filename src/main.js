43845e39b2455f3385fa22742e5c2720f4c89ab4
Bofi1 <angelborque03github@gmail.com>
2025-06-30 13:27:20 -0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    document.getElementById("preload").style.display = "none";
+  }, 2200); // 2 segundos
+
+  setTimeout(() => {
+    document.getElementById("content").style.display = "block";
+  }, 2200); // 2 segundos
+});
