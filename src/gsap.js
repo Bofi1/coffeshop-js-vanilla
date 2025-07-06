a608a53964bb8e2aa8be807aa0ae60d03e05b2a8
Bofi1 <angelborque03github@gmail.com>
2025-07-06 12:31:56 -0700

    animations update

--- src/gsap.js
+++ src/gsap.js
@@ -1,3 +1,7 @@
+window.addEventListener("beforeunload", function () {
+  window.scrollTo(0, 0);
+});
+
 gsap.to("#first", {
   opacity: 0,
   duration: 0.5,
@@ -15,7 +19,59 @@ gsap.to("#second", {
   scrollTrigger: {
     trigger: "#second",
     start: "700px center",
-    toggleActions: `play none none reverse`,
+    end: "1000px center",
+    toggleActions: `play reverse play reverse`,
+    markers: true,
+  },
+});
+
+gsap.to("#qr", {
+  opacity: 1,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#qr",
+    start: "1200px center",
+    end: "1500px center",
+    toggleActions: `play reverse play reverse`,
+    markers: true,
+  },
+});
+
+gsap.to("#promos1", {
+  y: -20,
+  opacity: 1,
+  duration: 0.5,
+  scrollTrigger: {
+    trigger: "#promos1",
+    start: "1700px center",
+    end: "2000px center",
+    toggleActions: `play reverse play reverse`,
+    markers: true,
+  },
+});
+
+gsap.to("#promos2", {
+  y: -20,
+  opacity: 1,
+  duration: 0.6,
+  scrollTrigger: {
+    trigger: "#promos2",
+    start: "1700px center",
+    end: "2000px center",
+    toggleActions: `play reverse play reverse`,
+    markers: true,
+  },
+});
+
+gsap.to("#promos3", {
+  y: -20,
+  opacity: 1,
+  duration: 0.7,
+  scrollTrigger: {
+    trigger: "#promos3",
+    start: "1700px center",
+    end: "2000px center",
+    toggleActions: `play reverse play reverse`,
     markers: true,
   },
 });
